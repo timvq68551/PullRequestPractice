080b7e5a8844688346ee444a066514d6a88b32a9
awoodward <awoodward@gmail.com>
2024-03-20 18:11:58 -0700

    Added movie quote text file and python program to print it.

---
+++ s24_woodward.py
@@ -0,0 +1,11 @@
+FILE_NAME = 's24_woodward.txt'
+try:
+    with open(FILE_NAME, 'r') as file:
+        # Read file
+        contents = file.read()
+        # Print contents
+        print(contents)
+except FileNotFoundError:
+    print(f"Error: File '{FILE_NAME}' not found.")
+except Exception as e:
+    print(f"Error: {str(e)}")
