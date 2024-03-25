f4bf9cfe56890e92ebb40a1c15efcfccbebf515c
Fabiola Gonzalez <fab2163552@maricopa.edu>
2024-03-25 12:20:49 -0700

    Added a movie quote in python

---
+++ s24_gonzalez.py
@@ -0,0 +1,11 @@
+"""
+Fabiola Gonzalez
+Module 08 Programming Project
+
+This program prints a movie quote.
+
+"""
+
+print("In case I don't see you..." + " good afternoon, good evening, and good night.")
+print("-Truman Burbank")
+print("(The Truman Show, 1998)")
