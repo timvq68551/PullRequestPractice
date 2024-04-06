04d95f414b7d0f81c9c53d0c6aac5e7734451a32
Kada's PC <ada2160594@maricopa.edu>
2024-04-06 03:32:49 -0700

    new files to commit for cis class

---
+++ S24_Willoughby.md
@@ -0,0 +1,3 @@
+"Round up the usual suspects"
+- Renault
+(Casablanca, 1942)
---
+++ S24_Willoughy.py
@@ -0,0 +1,7 @@
+def main():
+    with open("S24_Willoughby.md", "r") as file:
+        quote = file.read()
+    print(quote)
+
+if __name__ == "__main__":
+    main()
