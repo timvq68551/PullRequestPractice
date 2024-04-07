c5ea906c0c05b932cc848d33be33b21df1f1d8f5
dil2128763 <dil2128763@maricopa.edu>
2024-04-06 22:37:42 -0700

    Commiting additional files

---
+++ s2_Morrow.py
@@ -0,0 +1,18 @@
+"""
+Dillon Morrow
+Module 08 Programming Assignment
+Part B
+
+This program prints a movie quote from a text file.
+"""
+FILE_NAME = 's2_Morrow.txt'
+try:
+    with open(FILE_NAME, 'r') as file:
+        # Read file
+        contents = file.read()
+        # Print contents
+        print(contents)
+except FileNotFoundError:
+    print(f"Error: File '{FILE_NAME}' not found.")
+except Exception as e:
+    print(f"Error: {str(e)}")
