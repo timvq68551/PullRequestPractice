a45b73eec7546954c816b79ae187a59544e580ee
eclipsion42 <danikabillietbmhs@gmail.com>
2025-11-09 13:39:06 -0700

    Dylan Thomas

---
+++ f25_Thomas.py
@@ -0,0 +1,4 @@
+print('"Do not go gentle into that good night,')
+print('Old age should burn and rave at close of day;')
+print('Rage, rage against the dying of the light."')
+print("— Dylan Thomas, 'Do Not Go Gentle into That Good Night', 1947")
